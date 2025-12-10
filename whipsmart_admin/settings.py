49fdc050711f341549c8a1ee5c704ca2102c12e9
ParthShindenovus <parth.s@novuscode.com>
2025-12-10 11:32:19 +0530

    changes in setting allowd all the host and remove postgres related code

--- whipsmart_admin/settings.py
+++ whipsmart_admin/settings.py
@@ -28,10 +28,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-z)$&r+fl&xy#w&_hnyx(1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-# Allow all hosts in development, restrict in production
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
-if DEBUG:
-    ALLOWED_HOSTS = ['*']  # Allow all hosts in development
+# Allow all hosts (both development and production)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=lambda v: ['*'] if v == '*' else [s.strip() for s in v.split(',')])
 
 
 # Application definition
@@ -115,27 +113,6 @@ if DB_ENGINE == 'django.db.backends.mysql':
                 'autocommit': True,
             },
             'CONN_MAX_AGE': 600,  # Connection pooling
-        }
-    }
-elif DB_ENGINE == 'django.db.backends.postgresql':
-    # PostgreSQL Configuration
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': config('DB_NAME', default='whipsmart_db'),
-            'USER': config('DB_USER', default='postgres'),
-            'PASSWORD': config('DB_PASSWORD', default=''),
-            'HOST': config('DB_HOST', default='localhost'),
-            'PORT': config('DB_PORT', default='5432', cast=int),
-            'CONN_MAX_AGE': 600,
-        }
-    }
-else:
-    # SQLite Configuration (default for development)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
